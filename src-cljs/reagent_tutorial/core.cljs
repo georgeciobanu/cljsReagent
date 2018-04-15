6315e2373d4dff6876baf51d8a5d18beefea4f37
George Ciobanu <geo.ciobanu@gmail.com>
2018-04-14 22:06:02 -0500

    Trying to make move and resize cpmopnents at editor level

--- src-cljs/reagent_tutorial/core.cljs
+++ src-cljs/reagent_tutorial/core.cljs
@@ -4,15 +4,15 @@
 
 (enable-console-print!)
 
-(def selected-component (atom ""))
+(def sel-palette-comp (atom ""))
 
 
 ;; The "database" of your client side UI.
 (def app-state
   (r/atom
    {:components
-    [{:id "b1" :caption "Button 1" :type "button" :x 180 :y 50}
-     {:id "b2" :caption "Button 2" :type "button" :x 20 :y 50}]}))
+    [{:id "b1" :caption "Button 1" :type "button" :x 180 :y 50 :width 100 :height 50}
+     {:id "b2" :caption "Button 2" :type "button" :x 180 :y 150 :width 100 :height 50}]}))
 
 (defn get-next-component-id [component-type]
   (inc (count (filter #( = (:type %) component-type) (@app-state :components)))))
@@ -24,12 +24,18 @@
 
 (defn add-component! [c x y]
   (case c
-    "Button" (update-components! conj {:id (str "Button " (get-next-component-id "button"))
+    "button" (update-components! conj {:id (str "b" (get-next-component-id "button"))
                                        :caption (str "Button " (get-next-component-id "button"))
                                        :x x
                                        :y y
-                                       :type "button"})
-    "Label" (update-components! conj {:id "l3" :caption "Label 3" :x x :y y})
+                                       :type "button"
+                                       :width 100
+                                       :height 50})
+    "label" (update-components! conj {:id (str "L" (get-next-component-id "label"))
+                                      :caption (str "Label " (get-next-component-id "label"))
+                                      :x x
+                                      :y y
+                                      :type "label"})
     ""))
 
 (defn remove-component! [cid]
@@ -53,10 +59,47 @@
 
 
 ;; UI components
+
+
+(defn resize-handler-mid-left [c]
+  [:div {:class "resize-handler"
+         :style {:top (/ (:height c) 2)
+                 :left -3}}])
+
+(defn resize-handler-mid-right [c]
+  (let [start-resize (atom {})]
+    (fn [c]
+      [:div {:class "resize-handler"
+             :style {:top (/ (:height c) 2)
+                     :left (- (:width c) 3)
+                     :cursor "ew-resize"}
+             :on-mouse-down #(reset! start-resize {:x (.-clientX %)})
+             :on-mouse-move #(if (seq @start-resize)
+                               (update-component! (:id c)
+                                                  {:width (+ (:width c) (- (.-clientX %) (:x @start-resize)))}))
+             :on-mouse-up #(reset! start-resize {})
+                                                   }])))
+
+(defn resize-handler-mid-top [c]
+  [:div {:class "resize-handler"
+         :style {:top -3
+                 :left (/ (:width c) 2)}}])
+
+(defn resize-handler-mid-bottom [c]
+  [:div {:class "resize-handler"
+         :style {:top (- (:height c) 3)
+                 :left (/ (:width c) 2)}}])
+
+
+
 (defn component [c]
   (let [start-move (atom {})]
     (fn [c]
-      [:div {:class "button-component" :style {:top (:y c) :left (:x c)}
+      [:div {:class "button-component" :style {:top (:y c)
+                                               :left (:x c)
+                                               :width (str (:width c) "px")
+                                               :height (str (:height c) "px")
+                                               }
               :on-mouse-down #(do
                                 (reset! start-move {:x (- (.-clientX %) (:x c))
                                                     :y (- (.-clientY %) (:y c))})
@@ -67,7 +110,12 @@
                                                :y (- (.-clientY %) (:y @start-move))}))
          :on-mouse-up #(reset! start-move {})
          :on-mouse-out #(reset! start-move {})}
-   [:span (:caption c)]])))
+       [resize-handler-mid-left c]
+       [resize-handler-mid-top c]
+       [resize-handler-mid-right c]
+       [resize-handler-mid-bottom c]
+       ;[:span (:caption c)]
+       ])))
 
 
 
@@ -76,12 +124,19 @@
    [:div {:class "component-sidebar"}
     [:div {:class "button-component-sidebar" :on-click #(do
                                                   (.log js/console "Selected button")
-                                                  (reset! selected-component "Button"))}]
-    [:div {:class "label-component-sidebar"}]]
-   [:div {:id "editor-canvas" :on-click #(do
-                                            (.log js/console  (str "Created " @selected-component (.-clientX %) (.-clientY %)))
-                                            (add-component! @selected-component (.-clientX %) (.-clientY %))
-                                            (reset! selected-component ""))}
+                                                  (reset! sel-palette-comp "button"))}]
+    [:div {:class "label-component-sidebar"  :on-click #(do
+                                                  (.log js/console "Selected label")
+                                                  (reset! sel-palette-comp "label"))}]]
+   [:div {:id "editor-canvas"
+          :on-mouse-down #(cond ;if the user selected a component from the comp palette, place it on the canvas
+                                (seq @sel-palette-comp) (do
+                                                            (.log js/console  (str "Created " @sel-palette-comp (.-clientX %) (.-clientY %)))
+                                                            (add-component! @sel-palette-comp (.-clientX %) (.-clientY %))
+                                                            (reset! sel-palette-comp ""))
+                                ;if there are selected component(s) on the canvas, move it/them
+                                 (seq @selected-component) (do
+                                                             (.log js/console ()}
     (for [c (:components @app-state)]
       [component c])
     ]])
